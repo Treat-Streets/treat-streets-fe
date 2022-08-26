664c9233c22bbb18ba36a63a3c364277ae31d337
Victoria Fox-Collis <victoriafoxc@gmail.com>
2022-08-26 16:11:57 -0600

    Import Router and create paths

---
+++ src/Components/LandingPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../Components/LandingPage.css'
+
+const LandingPage = () => {
+	return (
+		<div>
+			<h2>Im the Landig Page!</h2>
+		</div>
+	)
+}
+
+export default LandingPage;
