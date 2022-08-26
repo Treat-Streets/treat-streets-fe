29da457f0959c806a3fcc5e8e5ff25c236139be6
Megan Schuetz <mschuetz411@gmail.com>
2022-08-26 16:42:10 -0600

    add buttonsto landing page and links

--- src/Components/LandingPage.js
+++ src/Components/LandingPage.js
@@ -1,10 +1,23 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 import '../Components/LandingPage.css'
 
 const LandingPage = () => {
 	return (
 		<div>
-			<h2>Im the Landig Page!</h2>
+			<section>
+				<p>
+				Welcome to Treat Streets!<br></br>You can find houses passing out candy for Halloween in your area or add your home to the treat streets map.
+				</p>
+			</section>
+			<section>
+				<Link to='/Register'>
+					<button>Register Your House</button>
+				</Link>
+				<Link to='/Map'>
+					<button>View Treat Streets</button>
+				</Link>
+			</section>
 		</div>
 	)
 }
