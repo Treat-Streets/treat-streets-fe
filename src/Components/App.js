982c9ce8e73d32a1ffaf45feb929a0e5f4b911e5
Victoria Fox-Collis <victoriafoxc@gmail.com>
2022-08-26 15:53:17 -0600

    Import all components to App.js

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import React from 'react'
+import Header from '../Components/Header.js'
+import MapPage from '../Components/MapPage.js'
+import Nav from '../Components/Nav.js'
+import RegisterForm from '../Components/RegisterForm.js'
+import ThankYouPage from '../Components/ThankYouPage.js'
+import ZipCodeForm from '../Components/ZipCodeForm.js'
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+			<h1>HIII</h1>
+			<Nav />
+			<Header />
+			<ZipCodeForm />
+			<MapPage />
+			<RegisterForm />
+			<ThankYouPage />
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/Components/ThankYouPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../Components/ThankYouPage.css'
+
+const ThankYouPage = () => {
+	return (
+		<div>
+			<h2>Im the Thank You page! YEET!</h2>
+		</div>
+	)
+}
+
+export default ThankYouPage;
