9312bf076adbb3b84b0978fa45d203242a61501f
“Corinne <corinne.canavan@gmail.com>
2022-08-27 12:39:13 -0600

    Install Apollo

--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './Components/App'
 import { BrowserRouter } from 'react-router-dom'
+import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client'
+
+const client = new ApolloClient({
+  uri: 'https://treat-streets-be.herokuapp.com/',
+  cache: new InMemoryCache(),
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
