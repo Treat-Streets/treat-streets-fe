12ba2d361749c5185632d48e95465b69f6b7ea6d
“Corinne <corinne.canavan@gmail.com>
2022-08-27 15:16:54 -0600

    Create Location mutation

--- src/Components/App.js
+++ src/Components/App.js
@@ -10,27 +10,40 @@ import ZipCodeForm from '../Components/ZipCodeForm.js'
 import LandingPage from '../Components/LandingPage.js'
 import { useQuery, gql } from '@apollo/client'
 
+const CREATE_LOCATION = gql`
+	mutation CreateLocation($email: String!, $streetAddress: String!, $city: String!, $state: String!, $zipcode: Int!, $locationType: String!, $scarinessLevel: Int!, $description: String, $startTime: String!, $endTime: String!, $image: String) {
+		createLocation(input: {
+			email: $email,
+			streetAddress: $streetAddress,
+			city: $city,
+			state: $state,
+			zipcode: $zipcode,
+			locationType: $locationType,
+			scarinessLevel: $scarinessLevel,
+			description: $description,
+			startTime: $startTime,
+			endTime: $endTime,
+			image: $image
+		}) {
+			location {
+				streetAddress
+        city
+        state
+        zipcode
+        locationType
+        scarinessLevel
+        description
+        startTime
+        endTime
+        image
+			}
+		}
+	}	
+`;
+
 const App = () => {
 
-	const GET_LOCATIONS = gql`
-		query GetLocations {
-			locations {
-				user_id
-				email
-				street_address
-				city
-				state
-				zipcode
-				location_type
-				scariness_level
-				description
-				start_time
-				end_time
-				image
-			}
-		}	
-	`;
-	
+
   return (
     <div className="App">
 			<Nav />
