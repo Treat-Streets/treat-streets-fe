b53b9a96defef967ded561cf88b74335c225a174
“Corinne <corinne.canavan@gmail.com>
2022-08-27 11:49:04 -0600

    Add dropdown to form for property

--- src/Components/RegisterForm.js
+++ src/Components/RegisterForm.js
@@ -10,6 +10,19 @@ const RegisterForm = () => {
 					placeholder="Enter email here"
 					type="text"
 				/>
+				<input
+					name="address"
+					placeholder="Enter address"
+					type="text"
+				/>
+				<select name="propertyType">
+					<option value="default" disabled> Select Property Type </option>
+					<option value="house">House</option>
+					<option value="condo">Condo</option>
+					<option value="townhome">Townhome</option>
+					<option value="apartment">Apartment</option>
+					<option value="business">Business</option>
+				</select>
 			</form>
 		</div>
 	)
