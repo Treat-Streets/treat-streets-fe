2fa64e051956a491633bf9b07b5601c0761d8fc0
Victoria Fox-Collis <victoriafoxc@gmail.com>
2022-08-30 11:13:22 -0600

    Initial map build and render

--- src/Components/MapPage.js
+++ src/Components/MapPage.js
@@ -1,16 +1,46 @@
-import React from 'react'
+import React, {useState} from 'react'
+import ReactMapGL, { Mapper, Popup } from 'react-map-gl'
 import '../Components/MapPage.css'
 
 const MapPage = ({locationData}) => {
 
-	let mappedLocations = locationData.map(location => {
-		return location.streetAddress
+	const [viewport, setViewPort] = useState({
+		latitude: 39.7392,
+		longitude: -104.9903, 
+		width: "100vw", 
+		height: "100vh",
+		zoom: 10
 	})
+
+	// const [selectedHouse, setSelectedHouse] = useState(null)
+
+	// const mappedLocations = locationData.map(location => {
+	// 	return location.streetAddress
+	// })
+
+// 	useEffect(() => {
+//     if (map.current) return; // initialize map only once
+//     map.current = new mapboxgl.Map({
+//         container: mapContainer.current,
+//         style: 'mapbox://styles/mapbox/streets-v11',
+//         center: [lng, lat],
+//         zoom: zoom
+//     });
+// });
 	
 	return (
-		<div>
-			<h2>Im the Map Page!</h2>
-			<p>{mappedLocations}</p>
+
+		<div className="map-container">
+			<ReactMapGL className="map"
+				{...viewport}
+				mapboxAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+				mapStyle="mapbox://styles/mapbox/light-v10"
+				onViewportChange={viewport => {
+					setViewPort(viewport)
+				}}
+			>
+
+			</ReactMapGL>
 		</div>
 	)
 }
