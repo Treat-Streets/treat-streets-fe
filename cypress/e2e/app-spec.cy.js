8e7669aab6e57561641c840ff8776bae7704929a
“Corinne <corinne.canavan@gmail.com>
2022-09-03 13:22:23 -0600

    Add initial tests for landing page contents

--- cypress/e2e/app-spec.cy.js
+++ cypress/e2e/app-spec.cy.js
@@ -1,5 +1,20 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+import { aliasQuery, aliasMutation, hasOperationName } from '../utils/graphql-test-utils'
+
+describe('App', () => {
+  beforeEach(() => {
+    cy.intercept('POST', 'https://treat-streets-be.herokuapp.com/graphql', (req) => {
+      aliasQuery(req, 'locations')
+      aliasMutation(req, 'createLocation')
+    })
+    cy.visit('http://localhost:3000/')
+  })
+
+  it ('should render landing page contents', () => {
+    cy.url().should('eq', 'http://localhost:3000/')
+
+    cy.get('.welcome-header').contains('h2', 'TREAT STREETS')
+    cy.get('p').should('have.text', 'You can find houses passing out candy for Halloween in your area or add your home to the treat streets map.')
+
+    cy.get('.landing-page-buttons-container').find('button').should('have.length', '2')
   })
 })
