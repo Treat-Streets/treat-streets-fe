63e006508d29f2a6ff54c7c131daba47bc414a5d
“Corinne <corinne.canavan@gmail.com>
2022-09-03 13:48:09 -0600

    Remove commented out code

--- cypress/utils/graphql-test-utils.js
+++ cypress/utils/graphql-test-utils.js
@@ -1,6 +1,3 @@
-// utils/graphql-test-utils.js
-
-// Utility to match GraphQL mutation based on the operation name
 export const hasOperationName = (req, operationName) => {
   const { body } = req
   return (
@@ -8,14 +5,12 @@ export const hasOperationName = (req, operationName) => {
   )
 }
 
-// Alias query if operationName matches
 export const aliasQuery = (req, operationName) => {
   if (hasOperationName(req, operationName)) {
     req.alias = `gql${operationName}Query`
   }
 }
 
-// Alias mutation if operationName matches
 export const aliasMutation = (req, operationName) => {
   if (hasOperationName(req, operationName)) {
     req.alias = `gql${operationName}Mutation`
