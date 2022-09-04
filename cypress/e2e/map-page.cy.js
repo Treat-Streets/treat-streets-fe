09cd503b190e08628661370a5a1dd591271db055
“Corinne <corinne.canavan@gmail.com>
2022-09-03 19:18:15 -0600

    Try to stub intercept response

--- cypress/e2e/map-page.cy.js
+++ cypress/e2e/map-page.cy.js
@@ -3,8 +3,15 @@ import { aliasQuery, aliasMutation, hasOperationName } from '../utils/graphql-te
 describe('Map Page', () => {
   beforeEach(() => {
     cy.intercept('POST', 'https://treat-streets-be.herokuapp.com/graphql', (req) => {
+      const { body } = req
       aliasQuery(req, 'locations')
       aliasMutation(req, 'createLocation')
+      if (hasOperationName(req, 'locations')) {
+        req.alias = 'gqllocationsQuery'
+        req.reply({
+          fixture: "locations"
+        })
+      }
     })
     cy.visit('http://localhost:3000/')
     cy.contains('View Treat Streets').click()
@@ -16,5 +23,5 @@ describe('Map Page', () => {
     cy.get('.loading-gif').should('have.attr', 'src').should('include', '/static/media/pumpkin.d74dc1ee5efafdfa23e5.gif')
   })
 
-  
+
 })
