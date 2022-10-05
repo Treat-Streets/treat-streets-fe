57bfc4bde31c518038d64c38ce05549e4aa10ca9
Victoria Fox-Collis <victoriafoxc@gmail.com>
2022-10-04 21:14:41 -0600

    Update last sentence in about us paragraph

--- src/Components/AboutUs.js
+++ src/Components/AboutUs.js
@@ -15,7 +15,7 @@ const AboutUs = () => {
       <p className='title'>About Us</p>
       <section className='app-description'>
         <img className="pumpkin" src={pumpkin} alt="moving pumpkin"/>
-        <p className='description'>When the spooky night of Halloween comes around it can be hard to know which houses in which neighborhoods will have lights on and be giving out candy. Treat Streets was created to help parents and adults know where to take kids Trick-or-Treating. Our map helps you know which houses are participating in giving out candy in your area. Avoid aimless walking down streets with few lights on by using Treat Streets to know before hand which neighborhoods will many houses particicpating!</p>
+        <p className='description'>When the spooky night of Halloween comes around it can be hard to know which houses in which neighborhoods will have lights on and be giving out candy. Treat Streets was created to help parents and adults know where to take kids Trick-or-Treating. Our map helps you know which houses are participating in giving out candy in your area. Avoid aimlessly walking down streets with few lights on by using Treat Streets to know beforehand which neighborhoods will have the most houses particicpating!</p>
       </section>
       <p className='title'>Meet The Team</p>
       <section className='team-mems'>
